12a5a8d189e68a4826f5ffbad935701597320b3b
Demaria Woods <dwoods447@gmail.com>
2023-03-18 19:47:38 -0500

    moving around connect

--- server/app.ts
+++ server/app.ts
@@ -5,13 +5,18 @@ import createServer from './utils/server.js'
 import { defaultConfig } from './config/default.server.js'
 import connect from './utils/connect.js'
 
+const connectToDB = async () => {
+  return await connect()
+}
 const { app } = createServer()
 
-/* tslint:disable-next-line */
-app.listen(defaultConfig.PORT, async () => {
-  await connect()
-  console.log(`SERVER listening at  ${defaultConfig.HOST}:${defaultConfig.PORT}!`)
-})
+try {
+  connectToDB()
+  /* tslint:disable-next-line */
+  app.listen(defaultConfig.PORT, async () => {
+    console.log(`SERVER listening at  ${defaultConfig.HOST}:${defaultConfig.PORT}!`)
+  })
+} catch (err) {}
 
 export default {
   handler: app,
