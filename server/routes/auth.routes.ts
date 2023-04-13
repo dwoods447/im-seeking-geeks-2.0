c06f1b9cff04e86fc4b6bd89e31bce08a80e315a
Demaria Woods <dwoods447@gmail.com>
2023-04-13 00:36:25 -0500

    corrected routes

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import AuthController from '../controllers/AuthController.js'
 const authRoutes = function authRoutes(app: Express) {
   /* TODO[Demaria] -  Remove */
   app.get('/token', AuthController.getJwtToken)
-  app.post('/api/login', AuthController.userLogin)
+  app.post('/login', AuthController.userLogin)
   app.post('/register', AuthController.userRegistration)
   app.post('/logout', AuthController.userLogout)
   app.post('/check/username/unique', AuthController.checkUserNameUnique)
